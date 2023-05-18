40a3010f9b4de783d218ca31fda9a34ed54716d0
Bhaskar Kapri <bhaskarkapri07@gmail.com>
2023-05-18 16:08:05 +0530

    populate notes and users using mongoose populate method

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -3,29 +3,30 @@ const Note = require('../models/note')
 
 const User = require('../models/user')
 
-notesRouter.get('/',async (request,response) => {
-    const notes = await Note.find({})
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
+
     response.json(notes)
 })
 
-notesRouter.get('/:id',async (request,response) => {
+notesRouter.get('/:id', async (request, response) => {
     const note = await Note.findById(request.params.id)
-    if(note){
+    if (note) {
         response.json(note)
-    }else{
+    } else {
         response.status(404).end()
     }
 })
 
-notesRouter.post('/',async (request,response) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const user = await User.findById(body.userId)
 
     const note = new Note({
-        content:body.content,
-        important:body.important === undefined ? false: body.important,
-        user: user.id
+        content: body.content,
+        important: body.important === undefined ? false : body.important,
+        user: user.id,
     })
 
     const savedNote = await note.save()
@@ -35,24 +36,24 @@ notesRouter.post('/',async (request,response) => {
     response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request,response) => {
+notesRouter.delete('/:id', async (request, response) => {
     await Note.findByIdAndRemove(request.params.id)
     response.status(204).end
 })
 
-notesRouter.put('/:id',(request,response,next) => {
+notesRouter.put('/:id', (request, response, next) => {
     const body = request.body
 
     const note = {
-        content:body.content,
-        important: body.important
+        content: body.content,
+        important: body.important,
     }
 
-    Note.findByIdAndUpdate(request.params.id,note,{ new:true })
-        .then(updatedNote => {
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then((updatedNote) => {
             response.json(updatedNote)
-        }).catch(error => next(error))
+        })
+        .catch((error) => next(error))
 })
-
 
 module.exports = notesRouter
