f1799e9eddfbc36ccc43c98f3488e071126f7731
Bhaskar Kapri <bhaskarkapri07@gmail.com>
2023-06-10 12:12:55 +0530

    add more test

--- frontend/cypress/e2e/note_app.cy.js
+++ frontend/cypress/e2e/note_app.cy.js
@@ -1,9 +1,25 @@
 describe('Note app', function () {
-    it('front page can be opened', function () {
+    beforeEach(function () {
         cy.visit('http://localhost:3000')
+    })
+
+    it('front page can be opened', function () {
         cy.contains('Notes')
         cy.contains(
             'Note app, Department of Computer Science, University of Helsinki 2023'
         )
+    })
+
+    it('login form can be opened', function () {
+        cy.contains('log in').click()
+    })
+
+    it('user can login', function () {
+        cy.contains('log in').click()
+        cy.get('#username').type('genZbuddha')
+        cy.get('#password').type('salainen')
+        cy.get('#login-button').click()
+
+        cy.contains('supersuperuser logged in')
     })
 })
